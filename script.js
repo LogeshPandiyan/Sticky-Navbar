58cca187376b24750d83e37e0593fbb6a979e966
Logesh Pandiyan <logeshlp25@gmail.com>
2025-10-11 20:23:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
 const nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
+
+
 function fixNav() {
     if(window.scrollY > nav.offsetHeight + 150) {
         nav.classList.add('active')
-    } else {
+    }
+    
+ else {
         nav.classList.remove('active')
     }
 }
